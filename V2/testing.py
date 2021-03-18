bf4b406d6ab6655c3af82da52ed66ae635f775ad
Pasindu N. Samaranayake <54874939+Pasindu19970808@users.noreply.github.com>
2021-03-18 09:56:59 +0800

    reg ex to create player ids

--- V2/testing.py
+++ V2/testing.py
@@ -1,6 +1,14 @@
-class tester:
-    def __init__(self):
-        return None
-    def addadd(self,x1,x2):
-        y = x1 + x2
-        return y
+import bs4 as BeautifulSoup
+import urllib.request
+import pandas as pd
+import numpy as np
+import re
+
+
+url = "https://www.basketball-reference.com/leagues/NBA_2013_totals.html"
+html_content = urllib.request.urlopen(url)
+bsoup = BeautifulSoup.BeautifulSoup(html_content,'html.parser')
+year_list = bsoup.find_all('table',{'id':'totals_stats'})[0].find_all('tbody')[0].find_all('tr',class_ = lambda x: x!= 'thead')
+
+for row in year_list:
+        row_result = list(map(lambda x: x.get_text(),row))
